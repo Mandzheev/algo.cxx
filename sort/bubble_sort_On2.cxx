90ed07c8f320bd77a3d5017bc2f8b647c6095502
Mandzheev <mandzheevdmitriy@gmail.com>
2021-06-09 21:08:37 +0500

    1

--- sort/bubble_sort_On2.cxx
+++ sort/bubble_sort_On2.cxx
@@ -1,3 +1,12 @@
+/*************************************************
+ *  Time: Worst: n^2
+ *         Best: n
+ *
+ * Extra space: O(1)
+ *
+ * Stable: Yes
+ ************************************************/
+
 #include <iostream>
 
 using namespace std;
@@ -5,15 +14,27 @@ using namespace std;
 void swap(int *xp, int *yp) {
   int temp = *xp;
   *xp = *yp;
-  *yp=temp;
+  *yp = temp;
 }
 
+// An optimized version of Bubble Sort
 void bubble_sort(int arr[], int n) {
 
-  for (int i = 0; i < n - 1; i++)
-    for (int j = 0; j < n - i - 1; j++)
-      if (arr[j] > arr[j + 1])
+  bool swapped;
+  for (int i = 0; i < n - 1; i++) {
+
+    swapped = false;
+    for (int j = 0; j < n - i - 1; j++) {
+
+      if (arr[j] > arr[j + 1]) {
         swap(&arr[j], &arr[j + 1]);
+        swapped = true;
+      }
+    }
+    // If no two elements were swapped by inner loop, then break
+    if (swapped == false)
+      break;
+  }
 }
 
 void print_arr(int arr[], int n) {
@@ -25,6 +46,6 @@ int main() {
 
   int arr[] = {75, 44, 323, 23, 22, 2, 222, 67};
   int n = sizeof(arr) / sizeof(arr[0]);
-  bubble_sort(arr,n);
-  print_arr(arr,n);
+  bubble_sort(arr, n);
+  print_arr(arr, n);
 }
--- sort/selection_sortOn2.cxx
+++ sort/selection_sortOn2.cxx
@@ -1,39 +1,48 @@
+/*************************************************
+ *  Time: Worst: n^2
+ *       Average: n^2
+ *
+ * Extra space: O(1)
+ *
+ * Stable: Not(defalt case), but it can be made
+ ************************************************/
+
 #include <iostream>
 
 using namespace std;
 
 void swap(int *xp, int *yp) {
-    int temp = *xp;
-    *xp = *yp;
-    *yp = temp;
+  int temp = *xp;
+  *xp = *yp;
+  *yp = temp;
 }
 
 void selection_sort(int arr[], int n) {
-    int i,j,min;
+  int i, j, min;
 
-    for(i=0;i<n-1;i++) {
-        min =i;
+  for (i = 0; i < n - 1; i++) {
 
-        for(j=i+1;j<n;j++)
-            
-            if (arr[j]<arr[min])
-                min=j;
+    // Find the minimum element in unsorted array
+    min = i;
+    for (j = i + 1; j < n; j++)
 
-        swap(&arr[min], &arr[i]);
+      if (arr[j] < arr[min])
+        min = j;
 
-    }
+    // Swap the found min element with the first element
+    swap(&arr[min], &arr[i]);
+  }
 }
-    void print_arr(int arr[],int n) {
-        for (int i=0;i<n;i++)
-            cout<<arr[i]<<" | ";
 
-    }
-
+void print_arr(int arr[], int n) {
+  for (int i = 0; i < n; i++)
+    cout << arr[i] << " | ";
+}
 
 int main() {
 
   int arr[] = {22, 33, 23, 222, 54, 8};
   int n = sizeof(arr) / sizeof(arr[0]);
-  selection_sort(arr,n);
-  print_arr(arr,n);
+  selection_sort(arr, n);
+  print_arr(arr, n);
 }
